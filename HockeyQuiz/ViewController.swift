a75fe654127e512bdcefa8ebd02d68ad9364e5c1
KirTarasko <kir.tarasko@gmail.com>
2022-05-30 11:46:59 +0200

    Added viewControllers, DataManager, Model and some logic

--- HockeyQuiz/ViewController.swift
+++ HockeyQuiz/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    private var test: Test!
+    let questionManager = DataManager()
+    private var countOfAnswers = 0
+    private var indexRange = -1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
+    var question: String {
+        test.question
+    }
+    
+    var possibleAnswers : [String] {
+        test.possibleAnswers
+    }
+    
+    var totalAnswers: Int {
+        return countOfAnswers
+    }
+    
+    
+    var countOfQuestions: Int {
+        questionManager.questions.count - 1
+    }
+    
+    func refreshTest(_ index: Int) {
+        let testData = questionManager.questions[index]
+        
+        test = Test(question: testData.question, possibleAnswers: testData.possibleAnswers, rightAnswer: testData.rightAnswer)
+    }
+}
 
+
+extension ViewController {
+    
+    private func newQuestion() {
+         
+        if indexRange < countOfQuestions {
+             indexRange += 1
+         } else if indexRange == countOfQuestions {
+             showResult()
+         }
+         
+         refreshTest(indexRange)
+         question.text = question
+         
+         for index in 0..<possibleAnswers.count {
+             let possibleAnswer = possibleAnswers[index]
+             let button = buttonAnswers[index]
+             
+             button.setTitle(possibleAnswer, for: .normal)
+             
+             let totalProgress = Float(indexRange) / Float(countOfQuestions)
+             progressBar.setProgress(totalProgress, animated: true)
+         }
+     }
+    private func showResult() {
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
 }
 
